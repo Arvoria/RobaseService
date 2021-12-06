7f98388b52dd62a4a5d29b5df6d88a6bbdfbc485
pnlmon <oth@havingastroke.party>
2021-12-06 17:33:03 +0800

    Revert "Added :Authenticate"
    This reverts commit 4b4b88bfdc88a7abba60c0da45cf95e5854c1edd.

--- src/RobaseService/init.lua
+++ src/RobaseService/init.lua
@@ -1,11 +1,7 @@
 local Robase = require(script.Robase)
 local HttpService = game:GetService("HttpService")
 
-local RobaseService = {
-    AuthType = {
-        Legacy = "Legacy";
-    }
-}
+local RobaseService = { }
 RobaseService.BaseUrl = nil
 RobaseService.AuthKey = nil
 RobaseService.__index = RobaseService
@@ -18,31 +14,22 @@ local function fixTrailingSlashes(url)
     return url
 end
 
-function RobaseService.new(baseUrl)
+function RobaseService.new(baseUrl, token)
     if baseUrl == nil then
         error("Bad Argument 1 baseUrl expected, got nil")
+    elseif token==nil then
+        error("Bad Argument 2 token expected, got nil")
     end
 
     local self = setmetatable({}, RobaseService)
     self.BaseUrl = fixTrailingSlashes(baseUrl)
+    self.AuthKey = ".json?auth="..token
     return self
-end
-
-function RobaseService:Authenticate(authType, token)
-    if token == nil then
-        error("Bad Argument 2 token expected, got nil")
-    end
-    
-    if authType == self.AuthType.Legacy then
-        self.AuthKey = ".json?auth="..token
-    else
-        error(('Authenticate method "%s" is not supported'):format(authType))
-    end
 end
 
 function RobaseService:GetRobase(name, scope)
     if self.AuthKey==nil or self.AuthKey=="" then
-        error("You must authenticate RobaseService with an AuthKey to use the RobaseService API")
+        error("You must instantiate RobaseService with an AuthKey to use the RobaseService API")
     elseif self.BaseUrl==nil or self.BaseUrl=="" then
         error("You must instantiate RobaseService with a BaseUrl to use the RobaseService API")
     end
